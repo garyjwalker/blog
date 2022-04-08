f8b3bde51093745b95af17ed979182489a60d473
Gary Walker <garyjwalker42@gmail.com>
2022-04-08 00:48:12 -0500

    Challenge1 Display Hello on main page.

--- app.js
+++ app.js
@@ -14,19 +14,12 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home")
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
+// Start listening on port.
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
