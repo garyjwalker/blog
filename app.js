abfe1f4308de94f8fd312dbb153718e81663a392
Gary Walker <garyjwalker42@gmail.com>
2022-04-08 01:10:56 -0500

    Configure style.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home")
+  res.render("home", {message: homeStartingContent})
 })
 
 
